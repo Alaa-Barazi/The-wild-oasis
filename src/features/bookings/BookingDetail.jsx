b7d256610175869e45a20d580e71fc8d7f5e16b5
Alaa-Barazi <127497841+Alaa-Barazi@users.noreply.github.com>
2023-12-22 19:25:34 +0200

    modified:   src/App.jsx 	modified:   src/features/bookings/BookingDetail.jsx 	modified:   src/features/bookings/BookingRow.jsx 	modified:   src/features/bookings/useBooking.js 	modified:   src/features/check-in-out/CheckinBooking.jsx 	new file:   src/features/check-in-out/useCheckin.js 	new file:   src/pages/Checkin.jsx

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -11,6 +11,7 @@ import ButtonText from "../../ui/ButtonText";
 import Spinner from "../../ui/Spinner";
 import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "./usebooking";
+import { useNavigate } from "react-router-dom";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -20,11 +21,11 @@ const HeadingGroup = styled.div`
 
 function BookingDetail() {
   const { booking, isLoading } = useBooking();
-
-  const { status, id: bookingId } = booking;
-
+  const navigate = useNavigate();
   const moveBack = useMoveBack();
   if (isLoading) return <Spinner />;
+
+  const { status, id: bookingId } = booking;
 
   const statusToTagName = {
     unconfirmed: "blue",
@@ -42,9 +43,14 @@ function BookingDetail() {
         <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
       </Row>
 
-      {/* <BookingDataBox booking={booking} /> */}
+      <BookingDataBox booking={booking} />
 
       <ButtonGroup>
+        {status === "unconfirmed" && (
+          <Button onClick={() => navigate(`/checkin/${bookingId}`)}>
+            Check in
+          </Button>
+        )}
         <Button variation="secondary" onClick={moveBack}>
           Back
         </Button>
