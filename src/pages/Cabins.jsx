751cd76ab9a15aba571e8ff9d4a4800f39258df1
Alaa-Barazi <127497841+Alaa-Barazi@users.noreply.github.com>
2023-12-22 10:45:29 +0200

    modified:   src/features/bookings/BookingTable.jsx 	new file:   src/features/bookings/useBookings.js 	modified:   src/features/cabins/CabinTable.jsx 	new file:   src/features/cabins/CabinTableOperations.jsx 	modified:   src/pages/Bookings.jsx 	modified:   src/pages/Cabins.jsx 	modified:   src/services/apiBookings.js 	modified:   src/ui/Empty.jsx 	modified:   src/ui/Filter.jsx 	modified:   src/ui/Menus.jsx 	modified:   src/ui/Select.jsx 	new file:   src/ui/SortBy.jsx

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,18 +2,18 @@ import CabinTable from "../features/cabins/CabinTable";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import AddCabin from "../features/cabins/AddCabin";
+import CabinTanleOperations from "../features/cabins/CabinTableOperations";
 function Cabins() {
-
   return (
     <>
       <Row type="horizontal">
         <Heading as="h1">All cabins</Heading>
-        <p>Filter / Sort</p>
+        <CabinTanleOperations />
+        {/* <p>Filter / Sort</p> */}
       </Row>
       <Row>
         <CabinTable />
-<AddCabin />
-      
+        <AddCabin />
       </Row>
     </>
   );
