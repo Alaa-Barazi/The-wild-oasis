8f86551dc355ad04a81fab7b8ded8aa6c1a9244e
Alaa-Barazi <127497841+Alaa-Barazi@users.noreply.github.com>
2023-12-23 10:41:17 +0200

    modified:   src/features/authentication/LoginForm.jsx 	new file:   src/features/authentication/useLogin.js 	modified:   src/features/bookings/BookingDetail.jsx 	modified:   src/features/bookings/BookingRow.jsx 	new file:   src/features/bookings/useDeleteBooking.js 	modified:   src/features/check-in-out/CheckinBooking.jsx 	modified:   src/features/check-in-out/useCheckin.js 	new file:   src/features/check-in-out/useCheckout.js 	modified:   src/pages/Login.jsx 	new file:   src/services/apiAuth.js 	modified:   src/ui/Filter.jsx 	new file:   src/ui/FormRowVertical.jsx 	modified:   src/ui/Heading.jsx

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -12,6 +12,11 @@ import Spinner from "../../ui/Spinner";
 import { useMoveBack } from "../../hooks/useMoveBack";
 import { useBooking } from "./usebooking";
 import { useNavigate } from "react-router-dom";
+import { HiArrowUpOnSquare, HiTrash } from "react-icons/hi2";
+import { useCheckout } from "../check-in-out/useCheckout";
+import { useDeleteBooking } from "./useDeleteBooking";
+import Modal from "../../ui/Modal";
+import ConfirmDelete from "../../ui/ConfirmDelete";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -21,6 +26,8 @@ const HeadingGroup = styled.div`
 
 function BookingDetail() {
   const { booking, isLoading } = useBooking();
+  const { checkout, isCheckingOut } = useCheckout();
+  const { isDeleting, deleteBooking } = useDeleteBooking();
   const navigate = useNavigate();
   const moveBack = useMoveBack();
   if (isLoading) return <Spinner />;
@@ -51,6 +58,33 @@ function BookingDetail() {
             Check in
           </Button>
         )}
+        {status === "checked-in" && (
+          <Button
+            icon={<HiArrowUpOnSquare />}
+            onClick={() => checkout(bookingId)}
+            disabled={isCheckingOut}
+          >
+            Check out
+          </Button>
+        )}
+        <Modal>
+          <Modal.Open opens="delete">
+            <Button variation="danger" disabled={isDeleting}>
+              Delete booking
+            </Button>
+          </Modal.Open>
+          <Modal.Window name="delete">
+            <ConfirmDelete
+              resourceName="booking"
+              disabled={isDeleting}
+              onConfirm={() =>
+                deleteBooking(bookingId, {
+                  onSettled: () => navigate(-1),
+                })
+              }
+            />
+          </Modal.Window>
+        </Modal>
         <Button variation="secondary" onClick={moveBack}>
           Back
         </Button>
